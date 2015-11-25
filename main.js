e409ffd515bb8ac118b76470b6c86f7f5b915008
Luke Thompson <lthompson@infomedia.com.au>
2015-11-26 10:21:58 +1100

    Change filter to a function

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 angular.module('app').controller("MainController", function($scope, Sonnets){
     $scope.main = {
         title: 'Sonnets App',
+        sonnetFilterText: '',
         sonnets: Sonnets.getSonnets()
     };
 
     $scope.sonnetClicked = function(sonnet){
         alert('Sonnet: ' + sonnet.lines)
+    };
+
+    $scope.filterSonnets = function(value, index, array) {
+        if ($scope.main.sonnetFilterText && $scope.main.sonnetFilterText.length > 0) {
+            return value.number == $scope.main.sonnetFilterText;
+        } else {
+            return true;
+        }
     };
 });
